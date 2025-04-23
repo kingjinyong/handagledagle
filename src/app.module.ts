dc566bc30f2995d4cccd7f7bbb51acdaf2087560
kingjinyong <wlsdydwhswkf@gmail.com>
2025-04-23 15:12:09 +0900

    chore: TypeORM 연결 및 엔티티 생성테스트

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // .env 자동 로드
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.getOrThrow<string>('DATABASE_HOST'),
+        port: parseInt(config.getOrThrow<string>('DATABASE_PORT'), 10),
+        username: config.getOrThrow<string>('DATABASE_USERNAME'),
+        password: config.getOrThrow<string>('DATABASE_PASSWORD'),
+        database: config.getOrThrow<string>('DATABASE_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // 개발 시만 true
+        logging: config.get<string>('NODE_ENV') === 'development',
+      }),
+    }),
+  ],
 })
 export class AppModule {}
